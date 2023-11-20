4a77b314d0b7ee0eff63e5f2aacc908efa6a1c2d
NguyenAnhPhuongNam <98841702+NguyenAnhPhuongNam@users.noreply.github.com>
2023-11-20 19:00:23 +0000

    added sfml lib

--- Course_work_CMP303/main.cpp
+++ Course_work_CMP303/main.cpp
@@ -1,5 +1,16 @@
+#include <iostream>
+#include <SFML/Graphics.hpp>
 
 int main()
 {
+	sf::Window window(sf::VideoMode(800, 600), "Pong Clone");
+
+	while (window.isOpen())
+	{
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape))
+		{
+			window.close();
+		}
+	}
 	return 0;
 }
